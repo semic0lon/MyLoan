c433931380bb155fc4ce7fbcfe39d8918742f7e8
aekkasit <ivetoldu123456k.times@gmail.com>
2026-01-27 12:04:18 +0700

    add deploy route

--- app.py
+++ app.py
@@ -1,7 +1,36 @@
+import os
+from flask import request, abort
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello MyLoan Flask App"
+    return "adding deploy route"
+
+
+
+
+
+
+
+
+
+
+
+
+DEPLOY_KEY = "170459"  
+@app.route("/deploy")
+def deploy():
+    key = request.args.get("key")
+
+    if key != DEPLOY_KEY:
+        abort(403)
+
+    # ดึงโค้ดล่าสุด
+    os.system("cd C:\\MyLoan && git pull")
+
+    # สั่งให้ Task Flask รันใหม่
+    os.system('schtasks /run /tn "MyLoan Flask Server"')
+
+    return "Deploy success!"
