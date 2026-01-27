f6f415682efb16015e0436acf0066a97809cb3fc
aekkasit <ivetoldu123456k.times@gmail.com>
2026-01-27 22:59:00 +0700

    add migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myloan.db'
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     from .routes import main
     app.register_blueprint(main)
